a74754917e3cf13685ad3002c4dba4320974b0f6
NgocLinhHub <21045371.linh@student.iuh.edu.vn>
2024-08-15 17:23:46 +0700

    test

---
+++ src/Welcome.java
@@ -0,0 +1,6 @@
+public class Welcome {
+    public static void main(String[] args) {
+        System.out.println("Hello Ngoc Linh");
+        System.out.println("Welcome to the DHHTTT19A!");
+    }
+}
